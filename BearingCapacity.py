6aebbb377eb32fdf4b6a1b245fff14d7fc25beb5
Marguez <59306658+Marguez@users.noreply.github.com>
2025-10-06 22:30:14 +0800

    Update BearingCapacity.py

--- BearingCapacity.py
+++ BearingCapacity.py
@@ -103,6 +103,7 @@ if d_wt < d_f+B:
     q = γ_s* d_f
     H = 0.5*B*math.tan(math.radians(45+idx/2))
     γ = γ_s/ (H**2)*(2*H-(d_wt-d_f))+(γ_sat -γ_s)/(H**2)*(H-(d_wt-d_f))**2
+    st.write(f"H = {H:.2f} m.")
 else:
   q= γ_s * d_f
   γ = γ_s
